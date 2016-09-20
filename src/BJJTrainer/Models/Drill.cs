45a935077075893042af12db7e1f0d261b43303d
Ben Logue <benjamin.logue73@gmail.com>
2016-09-20 11:20:48 -0700

    add models drill and routine.

---
+++ src/BJJTrainer/Models/Drill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BJJTrainer.Models
+{
+    [Table("Drills")]
+    public class Drill
+    {
+        [Key]
+        public int DrillId { get; set; }
+        public string Description { get; set; }
+        public int Time { get; set; }
+        public int TechniqueId { get; set; }
+        public virtual Technique Technique { get; set; }
+
+        public int RoutineId { get; set; }
+        public virtual Routine Routine { get; set; }
+    }
+}
---
+++ src/BJJTrainer/Models/Routine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BJJTrainer.Models
+{
+    [Table("Routines")]
+    public class Routine
+    {
+        public int RoutineId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Drill> Drills { get; set; }
+    }
+}
