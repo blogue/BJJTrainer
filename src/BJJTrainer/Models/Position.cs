78869d8ccc0683924a50269c8d3c3aa1166795ec
Ben Logue <benjamin.logue73@gmail.com>
2016-09-20 09:36:19 -0700

    Add models for Category, Position, and Technique.

---
+++ src/BJJTrainer/Models/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BJJTrainer.Models
+{
+    [Table("Positions")]
+    public class Position
+    {
+        [Key]
+        public int PositionId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Technique> Techniques { get; set; }
+    }
+}
---
+++ src/BJJTrainer/Models/Technique.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BJJTrainer.Models
+{
+    [Table("Techniques")]
+    public class Technique
+    {
+        [Key]
+        public int TechniqueId { get; set; }
+        public string Name { get; set; }
+        public int PositionId { get; set; }
+        public virtual Position Position { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
